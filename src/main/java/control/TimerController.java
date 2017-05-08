192a7b064a18f3f289970d6728613630129cf9f9
linzhouzhi <490664646@qq.com>
2017-05-08 22:12:33 +0800

    modal

--- src/main/java/control/TimerController.java
+++ src/main/java/control/TimerController.java
@@ -5,13 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Created by lzz on 17/5/7.
+ * Created by lzz on 17/5/8.
  */
 @Controller
-public class TreeController {
-    @RequestMapping("/tree/admin")
-    public String treeAdmin(Model model) {
-        return "tree_admin";
+public class TimerController {
+    @RequestMapping("/tree/timer")
+    public String timer(Model model) {
+        return "timer";
     }
-
 }
